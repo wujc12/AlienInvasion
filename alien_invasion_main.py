c608ade3fb4b62a9f1255d92d6c412a0150c2648
wujc12 <wujc12@live.com>
2018-08-21 14:47:36 +0800

    add utils.settings and modifies main script

--- alien_invasion_main.py
+++ alien_invasion_main.py
@@ -1,14 +1,16 @@
 import sys
 import pygame
+from utils.settings import Settings
 
 
 def run_game():
     # initialize game and create a display object
     pygame.init()
-    screen = pygame.display.set_mode((1200,800))
+    ai_settings = Settings()
+    screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
     # set background color
-    bg_color = (230,230,230)
+    bg_color = (230, 230, 230)
 
     # game loop
     while True:
